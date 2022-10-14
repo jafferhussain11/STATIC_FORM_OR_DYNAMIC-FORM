ef686f9c7298fd26bf1a4cd9de1a0c626929fb03
jafferhussain11 <jafferhussain11@gmail.com>
2022-10-14 15:16:11 +0530

    FINALDOM MANIP FORM

--- scriptt.js
+++ scriptt.js
@@ -1,16 +1,56 @@
-
-
 const form = document.getElementById('form1');
 //const name = form.ge
 form.addEventListener('submit',saveContent);
-
+const ul = document.getElementById('userlist');
 
 function saveContent(event){
 
             event.preventDefault();
-            localStorage.setItem('name',event.target.name.value);
-            localStorage.setItem('Email ID',event.target.eid.value);
-            localStorage.setItem('phone num',event.target.pno.value);
-            localStorage.setItem('Date',event.target.dt.value);
+            let myobj = {
+
+                name : event.target.name.value,
+                email : event.target.eid.value,
+                phone : event.target.pno.value,
+                date : event.target.dt.value
+
+            }
             
+            let myobj_serial = JSON.stringify(myobj);
+            localStorage.setItem(myobj.email,myobj_serial); 
+            displayUser(myobj);
+ }
+
+function  displayUser(user){
+
+         
+         const newlistitem = document.createElement('li');
+         newlistitem.setAttribute('data-email',user.email);
+         newlistitem.innerHTML = `${user.name} ${user.email} <button onclick = deleteUser('${user.email}')>Delete User</button>
+         <button onclick = editUser('${user.name}','${user.email}','${user.phone}')>Edit User</button>`;
+         ul.appendChild(newlistitem);
+
+
+}            //New div
+            
+function deleteUser(email){
+
+        const userTodel = ul.querySelector('[data-email="'+email+'"]');
+        //ddelete from local storage;
+        localStorage.removeItem(email);
+        //delete from UI
+        userTodel.remove();
 }
+            
+
+function editUser(name,email,phone){
+
+        document.getElementById('fname').value = name;
+        document.getElementById('eid').value = email;
+        document.getElementById('pno').value = phone;
+        //delete user
+        deleteUser(email);
+
+}
+            
+
+            
